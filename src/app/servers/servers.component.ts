6be696ab64f506c2decea82488a7df8b195dc0b7
Ansh Gandhi <anshgandhi16@gmail.com>
2021-10-31 21:22:35 -0400

    ngFor directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No Server was created!';
   serverName = "TestServer";
   serverCreated = false;
+  servers = ['TestServer1', 'TestServer2']
 
   constructor() {
     setTimeout( () => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
